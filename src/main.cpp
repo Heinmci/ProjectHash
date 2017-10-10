27c76d77777455259b4332dfa24c7f6153276492
Jules <dejardin@hotmail.co.uk>
2017-10-10 12:40:04 +0200

    Toujours en attente du commit de Julien

--- src/main.cpp
+++ src/main.cpp
@@ -7,27 +7,45 @@
 #include <stdlib.h>
 #include "sha.h"
 
+std::string argument_hash;
+std::string dict = "abcdefghijklmnopqrstuvwxyz 0123456789";
+
+void findPwd(std::string,int);
+bool matches_hash(std::string);
+
 int main( int argc, char *argv[] ) {
 
-	std::string hash_to_decode = argv[1];
-	int found = false;
-	char buffer [33];
+	argument_hash = argv[1];
 
-	for(int i = 97; i < 123; i++) {
-		char bob = i;
-		std::string s(1, bob);
-		//std::cout << bob << std::endl;
-		std::string test_hash = sha256(s);
-		//std::cout << test_hash << std::endl;
-		if (hash_to_decode == test_hash) {
-			std::cout << "Your secret word is :" << s << std::endl; 
-			found = true;
-		}
-
+	for(int i = 0; i < 100; i++) {
+		std::string word(i, '*'); 
+		findPwd(word,i);
 	}
-
-	if (!found) {
-		std::cout << "Didnt find secret word"<<std::endl;
-	}
-	
 }
+
+void findPwd(std::string word, int pos) {
+    if (pos < 0) {
+    	//std::cout << word << std::endl;
+    	bool is_match = matches_hash(word);
+		if (is_match) {
+			std::cout << "Your secret word is :" << word << std::endl; 
+			exit(EXIT_SUCCESS);
+		}
+        return;
+    }
+    for (int i = 0; i < dict.length(); i++) {
+    	word[pos] = dict[i];
+        findPwd(word, pos-1);
+    }
+}
+
+bool matches_hash(std::string word_to_test) {
+	std::string test_hash = sha256(word_to_test);
+	//std::cout << test_hash << std::endl;
+	if (argument_hash == test_hash) {
+		return true;
+	}
+
+	return false;
+}
+
